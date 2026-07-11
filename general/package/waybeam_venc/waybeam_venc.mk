fba8ed2af05f2f2a643a04b11f7412ab14e7b08b
JB <jbarth@bhome.lan>
2026-07-10 23:24:23 -0500

    build: fix waybeam_venc install path to out/star6e/waybeam

--- general/package/waybeam_venc/waybeam_venc.mk
+++ general/package/waybeam_venc/waybeam_venc.mk
@@ -14,7 +14,7 @@ define WAYBEAM_VENC_BUILD_CMDS
 endef
 
 define WAYBEAM_VENC_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/waybeam_venc $(TARGET_DIR)/usr/bin/waybeam_venc
+	$(INSTALL) -D -m 0755 $(@D)/out/star6e/waybeam $(TARGET_DIR)/usr/bin/waybeam_venc
 	$(INSTALL) -D -m 0755 $(WAYBEAM_VENC_PKGDIR)/S95waybeam $(TARGET_DIR)/etc/init.d/S95waybeam
 	$(INSTALL) -D -m 0644 $(WAYBEAM_VENC_PKGDIR)/waybeam.json $(TARGET_DIR)/etc/waybeam.json
 endef
